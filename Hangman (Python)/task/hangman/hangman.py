0c7c1f60086f6ae1ea2b74f5f7245512a6c73afe
micredis <micredis@gmail.com>
2023-08-01 15:13:43 -0700

    Add completed Stage 7/8: Error!

--- Hangman (Python)/task/hangman/hangman.py
+++ Hangman (Python)/task/hangman/hangman.py
@@ -1,14 +1,19 @@
 # Write your code here
 import random
 
-print("H A N G M A N")
-print()
+print("H A N G M A N\n")
+
 words = ["python", "java", "swift", "javascript"]
 secret_word = random.choice(words)
 lives = 8
+guessed_word = '-' * len(secret_word)
+guessed_letters = set()
 
 
 def reveal_character(secret_string, guessed_string, letter):
+    """
+    This function reveals the guessed character in the secret word.
+    """
     new_guessed_string = ''
     for s in range(len(secret_string)):
         if secret_string[s] == letter:
@@ -18,23 +23,33 @@ def reveal_character(secret_string, guessed_string, letter):
     return new_guessed_string
 
 
-guessed_word = '-' * len(secret_word)
 while lives > 0:
     print(guessed_word)
     character = input("Input a letter: > ")
-    if character not in secret_word:
+
+    if len(character) != 1:
+        print("Please, input a single letter.\n")
+        continue
+
+    if not character.isalpha() or not character.islower():
+        print("Please, enter a lowercase letter from the English alphabet.\n")
+        continue
+
+    if character in guessed_letters:
+        print("You've already guessed this letter.")
+    elif character not in secret_word:
         print("That letter doesn't appear in the word.")
-        lives -= 1
-    elif character in guessed_word:
-        print("No improvements.")
         lives -= 1
     else:
         guessed_word = reveal_character(secret_word, guessed_word, character)
-    print()
+
+    guessed_letters.add(character)
+
     if guessed_word == secret_word:
-        print(guessed_word)
-        print("You guessed the word!")
+        print(f"You guessed the word {guessed_word}!")
         print("You survived!")
         break
-else:
+    print()
+
+if lives == 0:
     print("You lost!")
