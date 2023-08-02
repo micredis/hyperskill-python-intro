106fd2ae93123808a464d916442940816494fad8
micredis <micredis@gmail.com>
2023-08-01 20:42:54 -0700

    Add completed Stage 8/8: Menu, please

--- Hangman (Python)/task/hangman/hangman.py
+++ Hangman (Python)/task/hangman/hangman.py
@@ -1,18 +1,35 @@
 # Write your code here
 import random
 
-print("H A N G M A N\n")
+WELCOME_PROMPT = 'Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: > '
+MENU_PROMPTS = ("play", "results", "exit")
+MAX_LIVES = 8
 
-words = ["python", "java", "swift", "javascript"]
-secret_word = random.choice(words)
-lives = 8
-guessed_word = '-' * len(secret_word)
-guessed_letters = set()
+
+def start_menu():
+    """
+    Start the game menu and handle user choices.
+    """
+    wins = 0
+    loses = 0
+    while True:
+        option = input(WELCOME_PROMPT)
+        if option == MENU_PROMPTS[0]:  # 'play'
+            print()
+            if start_game():  # if True, i.e. player wins
+                wins += 1
+            else:  # if player loses
+                loses += 1
+        elif option == MENU_PROMPTS[1]:  # 'results'
+            print(f"You won: {wins} times")
+            print(f"You lost: {loses} times")
+        elif option == MENU_PROMPTS[2]:  # 'exit'
+            break  # End the loop, and thus the function and the game
 
 
 def reveal_character(secret_string, guessed_string, letter):
     """
-    This function reveals the guessed character in the secret word.
+    Reveal the guessed character in the secret word.
     """
     new_guessed_string = ''
     for s in range(len(secret_string)):
@@ -23,33 +40,48 @@ def reveal_character(secret_string, guessed_string, letter):
     return new_guessed_string
 
 
-while lives > 0:
-    print(guessed_word)
-    character = input("Input a letter: > ")
+def start_game():
+    """
+    Start a game, return True if player wins, False otherwise.
+    """
+    words = ["python", "java", "swift", "javascript"]
+    secret_word = random.choice(words)
+    lives = MAX_LIVES
+    guessed_word = '-' * len(secret_word)
+    guessed_letters = set()
 
-    if len(character) != 1:
-        print("Please, input a single letter.\n")
-        continue
+    while lives > 0:
+        print(guessed_word)
+        character = input("Input a letter: > ")
 
-    if not character.isalpha() or not character.islower():
-        print("Please, enter a lowercase letter from the English alphabet.\n")
-        continue
+        if len(character) != 1:
+            print("Please, input a single letter.\n")
+            continue
 
-    if character in guessed_letters:
-        print("You've already guessed this letter.")
-    elif character not in secret_word:
-        print("That letter doesn't appear in the word.")
-        lives -= 1
-    else:
-        guessed_word = reveal_character(secret_word, guessed_word, character)
+        if not character.isalpha() or not character.islower():
+            print("Please, enter a lowercase letter from the English alphabet.\n")
+            continue
 
-    guessed_letters.add(character)
+        if character in guessed_letters:
+            print("You've already guessed this letter.")
+        elif character not in secret_word:
+            print("That letter doesn't appear in the word.")
+            lives -= 1
+        else:
+            guessed_word = reveal_character(secret_word, guessed_word, character)
 
-    if guessed_word == secret_word:
-        print(f"You guessed the word {guessed_word}!")
-        print("You survived!")
-        break
-    print()
+        guessed_letters.add(character)
 
-if lives == 0:
-    print("You lost!")
+        if guessed_word == secret_word:
+            print(f"\nYou guessed the word {guessed_word}!")
+            print("You survived!")
+            return True  # True is for win
+        print()
+
+    if lives == 0:
+        print("You lost!")
+        return False  # False is for loss
+
+
+print("H A N G M A N")
+start_menu()
